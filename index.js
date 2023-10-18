14dc4c939b5b18f277b6d24b454a847901c5e8f0
othmaneelbelghyty <145046059+othmaneelbelghyty@users.noreply.github.com>
2023-10-18 12:12:42 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let sentence = `short sentence delivers a powerful message with concise and effective language.
+ short sentence is a shanghai-based womenswear brand founded in 2015.
+  the brand pays homage to modern women through a collection of comfortable and timeless essentials.
+   commonplace and banal elements in everyday life inspire short sentence to design understated yet
+    evocative pieces  and pursue life-affirming fashion.`;
+
+let charactereCounter = 0;
+let wordCountere = 0;
+console.log(sentence.length);
+console.log(sentence);
+for (let i = 0; i < sentence.length; i++) {
+  if (sentence[i] != " ") {
+    charactereCounter++;
+  }
+}
+console.log(charactereCounter);
+for (let i = 0; i < sentence.length; i++) {
+  if (sentence[i] == " ") {
+    wordCountere++;
+  }
+}
+console.log(wordCountere);
